087ffb01ea801a633023e56bc45c86c5be17025c
chenjian120918 <1793886486@qq.com>
2017-08-07 15:11:18 +0800

    Signed-off-by: chenjian120918 <1793886486@qq.com>

--- DR/project/Chinese.py
+++
@@ -1,6 +0,0 @@
-#-*-coding:utf-8-*-
-#文件名: Chinese.py
-def set_ch():
-	from pylab import mpl
-	mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
-	mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
